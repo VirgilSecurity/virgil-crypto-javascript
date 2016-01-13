bf6c4d9503b399773fe33877e627f6e1b44a8842
Eugene Baranov <ebaranov.dev@gmail.com>
2016-01-14 02:24:10 +0300

    Fix asmjs updater script; Remove redundant dependencies;

--- scripts/download-asmjs-library.js
+++ scripts/download-asmjs-library.js
@@ -2,11 +2,9 @@ var fs = require('fs');
 var path = require('path');
 var del = require('del');
 var request = require('request');
-var targz = require('tar.gz');
-var recursiveReadDir = require('recursive-readdir');
 
-var downloadPath = path.resolve(path.join(__dirname, '../temp'));
-var downloadUrl = 'https://cdn.virgilsecurity.com/virgil-crypto/asmjs/virgil-crypto-1.2.0-asmjs.tar.gz';
+var downloadUrl = 'https://downloads.virgilsecurity.com/packages/asmjs/virgil-crypto-1.2.0-asmjs.js';
+var downloadFilePath = path.join(__dirname, '../', 'virgil-emscripten.js');
 var libPath = path.resolve(path.join(__dirname, '../src/lib/virgil-emscripten.js'));
 
 request
@@ -18,30 +16,20 @@ request
 	})
 	.on('error', abortWithError)
 	.on('end', function() {
-		if (fs.existsSync(downloadPath)) {
+		if (fs.existsSync(downloadFilePath)) {
 			console.log('The Virgil Crypto asmjs build successfully downloaded.\n\n');
 			console.log('Updating asmjs library...\n\n');
 
-			recursiveReadDir(downloadPath, function(err, fileNames) {
-				if (err) {
-					abortWithError();
-				} else {
-					var file = fileNames.find(function(fileName) {
-						return fileName.match(/^.*\.js$/i);
-					});
+			fs.writeFileSync(libPath, fs.readFileSync(downloadFilePath));
 
-					fs.writeFileSync(libPath, fs.readFileSync(file));
+			console.log('The asmjs library updated successfully...\n\n');
 
-					console.log('The asmjs library updated successfully...\n\n');
-
-					del(downloadPath);
-				}
-			});
+			del(downloadFilePath);
 		} else {
 			abortWithError();
 		}
 	})
-	.pipe(targz().createWriteStream(downloadPath));
+	.pipe(fs.createWriteStream(downloadFilePath));
 
 function abortWithError(error) {
 	console.log('Something goes wrong, the Virgil Crypto asmjs build is not downloaded yet.');

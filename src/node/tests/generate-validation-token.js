d280070956c4fa3a2c5489ccd9dd7c317622be7c
boo1ean <boo1ean0807@gmail.com>
2016-05-12 22:43:53 +0300

    Add test for checking custom validation token

--- src/node/tests/generate-validation-token.js
+++ src/node/tests/generate-validation-token.js
@@ -14,7 +14,16 @@ describe('generateValidationToken', () => {
 			keyPair.privateKey,
 			PASSWORD
 		);
+
 		expect(typeof validationToken).toEqual('string');
+
+		var decodedToken = new Buffer(validationToken, 'base64').toString('utf8');
+		var splited = decodedToken.split('.');
+		var uid = splited[0];
+		var sign = splited[1];
+		var signedData = uid + VirgilCrypto.IdentityTypesEnum.custom + IDENTITY_VALUE;
+
+		expect(VirgilCrypto.verify(signedData, keyPair.publicKey, new Buffer(sign, 'base64'))).toEqual(true);
 	});
 
 });

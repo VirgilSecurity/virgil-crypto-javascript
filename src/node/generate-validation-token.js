b218dceadb225abb02ec4fce3b31562c64d7b879
Vadim Avdeev <v.avdeiev@gmail.com>
2016-10-11 17:39:21 +0300

    Crypto v2 node: generateValidationToken accepts privateKey and password as Buffers

--- src/node/generate-validation-token.js
+++ src/node/generate-validation-token.js
@@ -1,21 +1,21 @@
-var _ = require('lodash');
 var uuid = require('node-uuid');
 var sign = require('./sign');
+var u = require('./utils');
 
 /**
  * Generate validation token
  */
 module.exports = function generateValidationToken (identityValue, identityType, privateKey, privateKeyPassword) {
-	if (!_.isString(identityValue)) {
-		throw new TypeError('identityValue must be a string');
-	}
+	privateKeyPassword = privateKeyPassword || new Buffer(0);
 
-	if (!_.isString(privateKey)) {
-		throw new TypeError('privateKey must be string');
+	if (typeof identityValue !== 'string') {
+		throw new TypeError('"identityValue" argument must be a string');
 	}
+	u.checkIsBuffer(privateKey, 'privateKey');
+	u.checkIsBuffer(privateKeyPassword, 'privateKeyPassword');
 
 	var uid = uuid.v4();
-	var signature = sign(uid + identityType + identityValue, privateKey, privateKeyPassword);
+	var signature = sign(new Buffer(uid + identityType + identityValue), privateKey, privateKeyPassword);
 	var validationToken = Buffer.concat([new Buffer(uid), new Buffer('.'), new Buffer(signature.toString('base64'))]);
 	return validationToken.toString('base64');
 };
--- src/node/tests/generate-validation-token.js
+++ src/node/tests/generate-validation-token.js
@@ -2,28 +2,45 @@
 var VirgilCrypto = require('../');
 var expect = require('expect');
 
-var PASSWORD = 'veryStrongPa$$0rd';
+var PASSWORD = new Buffer('veryStrongPa$$0rd');
 var IDENTITY_VALUE = 'email@example.com';
+var IDENTITY_TYPE = 'email';
 
 describe('generateValidationToken', function () {
-	it('Validation token is generated', function () {
+	it('should generate validation token with plain key', function () {
+		var keyPair = VirgilCrypto.generateKeyPair();
+		var validationToken = VirgilCrypto.generateValidationToken(
+			IDENTITY_VALUE,
+			IDENTITY_TYPE,
+			keyPair.privateKey
+		);
+
+		expect(typeof validationToken).toEqual('string');
+
+		validateToken(validationToken, keyPair.publicKey);
+	});
+
+	it('should generate validation token with encrypted key', function () {
 		var keyPair = VirgilCrypto.generateKeyPair({ password: PASSWORD });
 		var validationToken = VirgilCrypto.generateValidationToken(
 			IDENTITY_VALUE,
-			VirgilCrypto.IdentityTypesEnum.custom,
+			IDENTITY_TYPE,
 			keyPair.privateKey,
 			PASSWORD
 		);
 
 		expect(typeof validationToken).toEqual('string');
 
-		var decodedToken = new Buffer(validationToken, 'base64').toString('utf8');
-		var splited = decodedToken.split('.');
-		var uid = splited[0];
-		var sign = splited[1];
-		var signedData = uid + VirgilCrypto.IdentityTypesEnum.custom + IDENTITY_VALUE;
-
-		expect(VirgilCrypto.verify(signedData, keyPair.publicKey, new Buffer(sign, 'base64'))).toEqual(true);
+		validateToken(validationToken, keyPair.publicKey);
 	});
-
 });
+
+function validateToken(validationToken, publicKey) {
+	const decodedToken = new Buffer(validationToken, 'base64').toString('utf8');
+	const parts = decodedToken.split('.');
+	const uid = parts[0];
+	const sign = parts[1];
+	const signedData = new Buffer(uid + IDENTITY_TYPE + IDENTITY_VALUE);
+
+	expect(VirgilCrypto.verify(signedData, new Buffer(sign, 'base64'), publicKey)).toEqual(true);
+}

800b52c85a9b5c09e909d0f6ca3cd8c2cbbbd5d3
Vadim Avdeev <v.avdeiev@gmail.com>
2017-01-10 10:09:03 +0200

    add PEM to DER key format conversion tests

--- src/node/tests/keys-format-conversion.spec.js
+++ src/node/tests/keys-format-conversion.spec.js
@@ -23,5 +23,25 @@ describe('keys PEM - DER conversion', function () {
 		var decryptedData = VirgilCrypto.decrypt(encryptedData, recipientId, keyPair.privateKey);
 		expect(decryptedData.equals(plaintext)).toBe(true);
 	});
+
+	it('public key DER to DER conversion is noop', function () {
+		var firstPublicKeyDER = VirgilCrypto.publicKeyToDER(keyPair.publicKey);
+		var secondPublicKeyDER = VirgilCrypto.publicKeyToDER(firstPublicKeyDER);
+		expect(secondPublicKeyDER.equals(firstPublicKeyDER)).toBe(true);
+	});
+
+	it('private key DER to DER conversion is noop', function () {
+		var firstPrivateKeyDER = VirgilCrypto.privateKeyToDER(keyPair.privateKey);
+		var secondPrivateKeyDER = VirgilCrypto.privateKeyToDER(firstPrivateKeyDER);
+		expect(secondPrivateKeyDER.equals(firstPrivateKeyDER)).toBe(true);
+	});
+
+	it('extract returns pubkey in same format', function () {
+		var pubkey = VirgilCrypto.extractPublicKey(keyPair.privateKey);
+		var pubkeyDER = VirgilCrypto.publicKeyToDER(pubkey);
+
+		expect(pubkey.toString('utf8').indexOf('BEGIN') > 0).toBe(true);
+		expect(pubkey.equals(pubkeyDER)).toBe(false);
+	});
 });
 

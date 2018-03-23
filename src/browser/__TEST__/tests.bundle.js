92c9abe01bc63f97ce138b682d7bdc3c649dff8b
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-03-23 15:23:20 +0200

    get rid of async wrappers

--- src/browser/__TEST__/tests.bundle.js
+++ src/browser/__TEST__/tests.bundle.js
@@ -1,16 +1,11 @@
 import './namespace.spec';
 import './generate-key-pair.spec';
-import './generate-key-pair-async.spec';
 import './encrypt-decrypt.spec';
-import './encrypt-decrypt-async.spec';
 import './sign-verify.spec';
-import './sign-verify-async.spec';
 import './obfuscate.spec';
 import './hash.spec';
 import './keys-format-convert.spec';
 import './extract-public-key.spec';
 import './encrypt-decrypt-private-key.spec';
 import './sign-encrypt-decrypt-verify.spec';
-import './sign-encrypt-decrypt-verify-async.spec';
 import './decrypt-then-verify-multi-keys.spec';
-import './decrypt-then-verify-multi-keys-async.spec';
--- src/browser/utils/worker-crypto-context.js
+++
@@ -1,6 +0,0 @@
-Module = Module || {};
-Module.ENVIRONMENT = 'WORKER';
-Module.setDelayFunction(function (fn) {
-	setTimeout(fn, 0);
-});
-var VirgilCryptoWorkerContext = Module;

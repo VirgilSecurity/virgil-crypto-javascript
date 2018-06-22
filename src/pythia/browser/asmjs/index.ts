4b0b599ce04f6c949fdffaa69673bd8c49bf37be
Vadim Avdeiev <v.avdeiev@gmail.com>
2018-06-22 19:21:19 +0300

    feat: allow amount of memory used in browser to be configured by user

--- src/pythia/browser/asmjs/index.ts
+++ src/pythia/browser/asmjs/index.ts
@@ -1,6 +1,13 @@
 import './virgil_crypto_asmjs.js';
 
-const lib = __virgilCrypto();
+const minMemory = 1024 * 1024 * 16; // 16 MB - emscripten default
+
+let totalMemory = parseInt((window as any).__VIRGIL_CRYPTO_TOTAL_MEMORY_BYTES__, 10);
+totalMemory = isNaN(totalMemory) ? minMemory : Math.max(totalMemory, minMemory);
+
+const lib = __virgilCrypto({
+	TOTAL_MEMORY: totalMemory
+});
 lib.setDelayFunction(function (delayed: Function) {
 	setTimeout(delayed, 0);
 });

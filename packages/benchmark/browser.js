708043af56f586f832dff5beb13fc62857c15101
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-08-14 13:50:05 +0300

    Update benchmarks

--- packages/benchmark/browser.js
+++ packages/benchmark/browser.js
@@ -1,3 +1,10 @@
 const runBenchmark = require('./benchmark');
 
-runBenchmark(window.Benchmark);
+const log = str => {
+  const div = document.createElement('div');
+  const textNode = document.createTextNode(str);
+  div.appendChild(textNode);
+  document.body.appendChild(div);
+};
+
+runBenchmark(window.Benchmark, log);
--- packages/benchmark/node.js
+++ packages/benchmark/node.js
@@ -1,4 +1,17 @@
-const benchmark = require('benchmark');
-const runBenchmark = require('./benchmark');
+const benchmark = require("benchmark");
+const fs = require("fs");
+const path = require("path");
 
-runBenchmark(benchmark);
+const runBenchmark = require("./benchmark");
+
+const outputPath = path.join(__dirname, "node.txt");
+
+if (fs.existsSync(outputPath)) {
+  fs.unlinkSync(outputPath);
+}
+
+const createLog = logPath => str => fs.appendFileSync(logPath, `${str}\n`);
+
+const benchmarkLog = createLog(outputPath);
+
+runBenchmark(benchmark, benchmarkLog);

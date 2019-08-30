5ebeaa2f4d52a73af29bc7605f4dcdcdfac54d13
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-08-30 13:07:52 +0300

    Fix types in data-utils package

--- packages/data-utils/src/toBuffer.ts
+++ packages/data-utils/src/toBuffer.ts
@@ -1,6 +1,8 @@
+import { NodeBuffer } from './types';
+
 export const createToBufferFunction = (NodeBuffer: any) => (
   array: Uint8Array
-) => {
+): NodeBuffer => {
   let buffer = NodeBuffer.from(array.buffer);
   if (array.byteLength !== array.buffer.byteLength) {
     buffer = buffer.slice(array.byteOffset, array.byteOffset + array.byteLength);

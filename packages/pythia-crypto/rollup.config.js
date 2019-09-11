3af6f08a63e08f5abc966558a08756a7777aab0a
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-09-11 20:12:09 +0300

    Simplify pythia-crypto build process

--- packages/pythia-crypto/rollup.config.js
+++ packages/pythia-crypto/rollup.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 
+const builtinModules = require('builtin-modules');
 const commonjs = require('rollup-plugin-commonjs');
+const copy = require('rollup-plugin-copy');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const replace = require('rollup-plugin-re');
 const { terser } = require('rollup-plugin-terser');
@@ -8,84 +10,124 @@ const typescript = require('rollup-plugin-typescript2');
 
 const packageJson = require('./package.json');
 
-const dependencies = Object.keys(packageJson.dependencies);
-
 const FORMAT = {
   CJS: 'cjs',
   ES: 'es',
   UMD: 'umd',
 };
 
-const sourcePath = path.join(__dirname, 'src');
-const outputPath = path.join(__dirname, 'dist');
-const input = path.join(sourcePath, 'index.ts');
+const CRYPTO_TYPE = {
+  WASM: 'wasm',
+  ASMJS: 'asmjs',
+};
 
-const createNonBundledEntry = (format, pythiaLibrary) => ({
-  input,
-  external: dependencies.concat([pythiaLibrary]),
+const TARGET = {
+  BROWSER: 'browser',
+  WORKER: 'worker',
+  NODE: 'node',
+};
+
+const sourceDir = path.join(__dirname, 'src');
+const outputDir = path.join(__dirname, 'dist');
+
+const getOutputFilename = (target, cryptoType, format) =>
+  `${target}${cryptoType === CRYPTO_TYPE.ASMJS ? '.asmjs' : ''}.${format}.js`;
+
+const getCryptoEntryPointName = (target, cryptoType) =>
+  `${target}${cryptoType === CRYPTO_TYPE.ASMJS ? '.asmjs' : ''}.es.js`;
+
+const createBrowserEntry = (target, cryptoType, format) => ({
+  input: path.join(sourceDir, 'index.ts'),
   output: {
     format,
-    file: path.join(outputPath, `${path.parse(pythiaLibrary).name}.js`),
-  },
-  plugins: [
-    replace({
-      patterns: [
-        {
-          match: /initPythia.ts$/,
-          test: '@virgilsecurity/core-pythia',
-          replace: pythiaLibrary,
-        },
-      ],
-    }),
-    typescript({
-      exclude: ['**/*.test.ts'],
-      useTsconfigDeclarationDir: true,
-    }),
-  ],
-});
-
-const createUmdEntry = (pythiaLibrary, outputFilename) => ({
-  input,
-  output: {
-    format: 'umd',
-    file: path.join(outputPath, outputFilename),
+    file: path.join(outputDir, getOutputFilename(target, cryptoType, format)),
     name: 'VirgilPythiaCrypto',
   },
   plugins: [
     replace({
       patterns: [
         {
-          match: /initPythia.ts$/,
+          match: /(initPythia|types)\.ts$/,
           test: '@virgilsecurity/core-pythia',
-          replace: pythiaLibrary,
+          replace: path.join(
+            '@virgilsecurity',
+            'core-pythia',
+            getCryptoEntryPointName(target, cryptoType),
+          ),
         },
       ],
     }),
-    nodeResolve({ browser: true }),
+    nodeResolve({ browser: true, extensions: ['.js', '.ts'] }),
+    commonjs(),
+    typescript({
+      exclude: ['**/*.test.ts'],
+      objectHashIgnoreUnknownHack: true,
+      useTsconfigDeclarationDir: true,
+    }),
+    cryptoType === CRYPTO_TYPE.WASM &&
+      copy({
+        targets: [
+          {
+            src: path.join(
+              __dirname,
+              'node_modules',
+              '@virgilsecurity',
+              'core-pythia',
+              `libpythia.${target}.wasm`,
+            ),
+            dest: outputDir,
+          },
+        ],
+      }),
+    format === FORMAT.UMD && terser(),
+  ],
+});
+
+const createNodeJsEntry = (cryptoType, format) => ({
+  input: path.join(sourceDir, 'index.ts'),
+  output: {
+    format,
+    file: path.join(outputDir, getOutputFilename(TARGET.NODE, cryptoType, format)),
+  },
+  external: builtinModules.concat(Object.keys(packageJson.dependencies)),
+  plugins: [
+    replace({
+      patterns: [
+        {
+          match: /(initPythia|types)\.ts$/,
+          test: '@virgilsecurity/core-pythia',
+          replace: path.join(
+            '@virgilsecurity',
+            'core-pythia',
+            getCryptoEntryPointName(TARGET.NODE, cryptoType),
+          ),
+        },
+      ],
+    }),
+    nodeResolve({ extensions: ['.js', '.ts'] }),
     commonjs(),
     typescript({
       exclude: ['**/*.test.ts'],
       useTsconfigDeclarationDir: true,
     }),
-    terser(),
   ],
 });
 
 module.exports = [
-  createUmdEntry('@virgilsecurity/core-pythia/browser.asmjs.es.js', 'browser.asmjs.umd.js'),
-  createUmdEntry('@virgilsecurity/core-pythia/browser.es.js', 'browser.umd.js'),
-  createUmdEntry('@virgilsecurity/core-pythia/worker.asmjs.es.js', 'worker.asmjs.umd.js'),
-  createUmdEntry('@virgilsecurity/core-pythia/worker.es.js', 'worker.umd.js'),
-  createNonBundledEntry(FORMAT.CJS, '@virgilsecurity/core-pythia/browser.asmjs.cjs.js'),
-  createNonBundledEntry(FORMAT.ES, '@virgilsecurity/core-pythia/browser.asmjs.es.js'),
-  createNonBundledEntry(FORMAT.CJS, '@virgilsecurity/core-pythia/browser.cjs.js'),
-  createNonBundledEntry(FORMAT.ES, '@virgilsecurity/core-pythia/browser.es.js'),
-  createNonBundledEntry(FORMAT.CJS, '@virgilsecurity/core-pythia/node.asmjs.cjs.js'),
-  createNonBundledEntry(FORMAT.ES, '@virgilsecurity/core-pythia/node.asmjs.es.js'),
-  createNonBundledEntry(FORMAT.CJS, '@virgilsecurity/core-pythia/node.cjs.js'),
-  createNonBundledEntry(FORMAT.ES, '@virgilsecurity/core-pythia/node.es.js'),
-  createNonBundledEntry(FORMAT.CJS, '@virgilsecurity/core-pythia/worker.asmjs.cjs.js'),
-  createNonBundledEntry(FORMAT.ES, '@virgilsecurity/core-pythia/worker.asmjs.es.js'),
-  createNonBundledEntry(FORMAT.CJS, '@virgilsecurity/core-pythia/worker.cjs.js'),
-  createNonBundledEntry(FORMAT.ES, '@virgilsecurity/core-pythia/worker.es.js'),
+  createBrowserEntry(TARGET.BROWSER, CRYPTO_TYPE.ASMJS, FORMAT.CJS),
+  createBrowserEntry(TARGET.BROWSER, CRYPTO_TYPE.ASMJS, FORMAT.ES),
+  createBrowserEntry(TARGET.BROWSER, CRYPTO_TYPE.ASMJS, FORMAT.UMD),
+  createBrowserEntry(TARGET.BROWSER, CRYPTO_TYPE.WASM, FORMAT.CJS),
+  createBrowserEntry(TARGET.BROWSER, CRYPTO_TYPE.WASM, FORMAT.ES),
+  createBrowserEntry(TARGET.BROWSER, CRYPTO_TYPE.WASM, FORMAT.UMD),
+  createNodeJsEntry(CRYPTO_TYPE.ASMJS, FORMAT.CJS),
+  createNodeJsEntry(CRYPTO_TYPE.ASMJS, FORMAT.ES),
+  createNodeJsEntry(CRYPTO_TYPE.WASM, FORMAT.CJS),
+  createNodeJsEntry(CRYPTO_TYPE.WASM, FORMAT.ES),
+  createBrowserEntry(TARGET.WORKER, CRYPTO_TYPE.ASMJS, FORMAT.CJS),
+  createBrowserEntry(TARGET.WORKER, CRYPTO_TYPE.ASMJS, FORMAT.ES),
+  createBrowserEntry(TARGET.WORKER, CRYPTO_TYPE.ASMJS, FORMAT.UMD),
+  createBrowserEntry(TARGET.WORKER, CRYPTO_TYPE.WASM, FORMAT.CJS),
+  createBrowserEntry(TARGET.WORKER, CRYPTO_TYPE.WASM, FORMAT.ES),
+  createBrowserEntry(TARGET.WORKER, CRYPTO_TYPE.WASM, FORMAT.UMD),
 ];
---
+++ packages/pythia-crypto/worker.cjs.js
@@ -0,0 +1,2 @@
+require('./dist/libpythia.browser.wasm');
+module.exports = require('./dist/worker.cjs');

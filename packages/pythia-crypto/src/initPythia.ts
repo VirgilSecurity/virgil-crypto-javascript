61a34173981856183d755eb95ce3c390b0d09b1b
Serhii Nanovskyi <serhii.nanovskyi@icloud.com>
2019-09-12 15:21:04 +0300

    Fix initCrypto and initPythia

--- packages/pythia-crypto/src/initPythia.ts
+++ packages/pythia-crypto/src/initPythia.ts
@@ -6,9 +6,8 @@ export async function initPythia() {
   try {
     getPythiaModules();
     console.warn('Pythia modules are already set. Further calls to `initPythia` are ignored.');
-  } catch (_) {
     return;
-  }
+  } catch (_) {}
   const pythiaModules = await initPythiaModules();
   setPythiaModules(pythiaModules);
 }
--- packages/virgil-crypto/src/index.ts
+++ packages/virgil-crypto/src/index.ts
@@ -9,9 +9,8 @@ export const initCrypto = async () => {
   try {
     getFoundationModules();
     console.warn('Foundation modules are already set. Further calls to `initCrypto` are ignored.');
-  } catch (_) {
     return;
-  }
+  } catch (_) {}
   const foundationModules = await initFoundation();
   setFoundationModules(foundationModules);
 };

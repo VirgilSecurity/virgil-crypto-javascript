0b209dce043b59fccf6045abf51caeaa7d834cf4
Serhii Nanovskyi <snanovskyi@icloud.com>
2020-01-21 18:38:48 +0200

    Update project structure & dependencies

--- packages/base-crypto/src/constants.ts
+++
@@ -1,5 +0,0 @@
-import { NodeBuffer } from '@virgilsecurity/data-utils';
-
-export const DATA_SIGNATURE_KEY = NodeBuffer.from('VIRGIL-DATA-SIGNATURE', 'utf8');
-
-export const DATA_SIGNER_ID_KEY = NodeBuffer.from('VIRGIL-DATA-SIGNER-ID', 'utf8');
